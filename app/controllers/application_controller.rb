f7f02587a8be44ec1b7de554abb1afbb1c38cdb7
bridgetnoelleee <bridgetlegaspi@gmail.com>
2016-06-24 15:16:36 +0800

    test commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
 
-  def hello
-    render text: "¡¡¡WOOOOHOOO HELLO WORLD PARE HOLA KUMUSTA!!!"
-  end
+	def hello
+		render text: "¡¡¡WOOOOHOOO HELLO WORLD PARE HOLA KUMUSTA!!!"
+	end
 
-  def goodbye
-    render text: "EZ CHALLENGE CHAROT GOODBYE BEH."
-  end
+	def goodbye
+		render text: "EZ CHALLENGE CHAROT GOODBYE BEH."
+	end
 end
